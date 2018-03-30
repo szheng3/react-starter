5185f0d127e9df46afb7e6aa1e8d494d027dec0a
Sept.Griffith <szheng3@uci.edu>
2018-03-29 20:31:12 -0700

    added the ssr

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,16 +1,16 @@
-import 'jquery';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { CounterButton, GithubButton, ProfileCard, ChartCard } from 'components';
+import { CounterButton, GithubButton, ProfileCard } from 'components';
 import config from 'config';
 import Helmet from 'react-helmet';
 import { connect } from 'react-redux';
 import 'mdbreact/dist/css/mdb.css';
 import avatar from 'assets/img/faces/marc.jpg';
 import { Grid } from 'material-ui';
-import { AccessTime } from 'material-ui-icons';
-import ChartistGraph from 'react-chartist';
+
+let Button,
+  Input;
 
 @connect(state => ({
   online: state.online
@@ -19,35 +19,26 @@ export default class Home extends Component {
   static propTypes = {
     online: PropTypes.bool.isRequired
   };
-  render() {
-    if (typeof window !== 'undefined') {
-      require('node-waves');
-      require('bootstrap');
-    }
-    const { Button, Input } = require('mdbreact');
-    // const { emailsSubscriptionChart } = typeof window !== 'undefined' ? require('variables/charts') : {};
 
+  constructor() {
+    super();
+    this.state = { showMap: false };
+  }
+
+  componentDidMount() {
+    require('node-waves');
+    require('bootstrap');
+    Button = require('mdbreact').Button;
+    Input = require('mdbreact').Input;
+    this.setState({ showMap: true });
+  }
+
+  render() {
     require('./home.css');
     const { online } = this.props;
     const styles = require('./Home.scss');
     // require the logo image both from client and server
     const logoImage = require('./logo.png');
-    const data = {
-      labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
-      series: [[1, 2, 4, 8, 6, -2, -1, -4, -6, -2]]
-    };
-
-    const options = {
-      high: 10,
-      low: -10,
-      axisX: {
-        labelInterpolationFnc(value, index) {
-          return index % 2 === 0 ? value : null;
-        }
-      }
-    };
-
-    const type = 'Bar';
 
     return (
       <div className={styles.home}>
@@ -62,35 +53,42 @@ export default class Home extends Component {
             <h1>{config.app.title}</h1>
 
             <h2>{config.app.description}</h2>
-            <Grid item xs={12} sm={12} md={6}>
-              <ChartCard
-                chart={<ChartistGraph data={data} options={options} type={type} />}
-                chartColor="orange"
-                title="Email Subscriptions"
-                text="Last Campaign Performance"
-                statIcon={AccessTime}
-                statText="campaign sent 2 days ago"
-              />
-            </Grid>
 
-            <Grid item xs={12} sm={12} md={4}>
-              <ProfileCard
-                avatar={avatar}
-                subtitle="CEO / CO-FOUNDER"
-                title="Alec Thompson"
-                description="Don't be scared of the
+            {this.state.showMap ? (
+              <form>
+                <p className="h5 text-center mb-4">Sign up</p>
+                <Input label="Your name" icon="user" group type="email" validate error="wrong" success="right" />
+                <Input label="Your email" icon="envelope" group type="email" validate error="wrong" success="right" />
+                <Input label="Your password" icon="lock" group type="password" validate />
+                <div className="text-center">
+                  <Button color="deep-orange">Sign up</Button>
+                </div>
+              </form>
+            ) : (
+              <div>Loading map</div>
+            )}
+
+            {this.state.showMap ? (
+              <Grid item xs={12} sm={12} md={4}>
+                <ProfileCard
+                  avatar={avatar}
+                  subtitle="CEO / CO-FOUNDER"
+                  title="Alec Thompson"
+                  description="Don't be scared of the
                  truth because we need to restart the human
                   foundation in truth And I love you like Kanye
                    loves Kanye I love Rick Owens’ bed design but the back is...
                   "
-                footer={
-                  <Button color="primary" round>
-                    Follow
-                  </Button>
-                }
-              />
-            </Grid>
-
+                  footer={
+                    <Button color="primary" round>
+                      Follow
+                    </Button>
+                  }
+                />
+              </Grid>
+            ) : (
+              <div>Loading map</div>
+            )}
             <p>
               <a
                 className={styles.github}
@@ -268,15 +266,6 @@ export default class Home extends Component {
 
           <h3>Features demonstrated in this project</h3>
           <h2 className="mb-5">Form register</h2>
-          <form>
-            <p className="h5 text-center mb-4">Sign up</p>
-            <Input label="Your name" icon="user" group type="email" validate error="wrong" success="right" />
-            <Input label="Your email" icon="envelope" group type="email" validate error="wrong" success="right" />
-            <Input label="Your password" icon="lock" group type="password" validate />
-            <div className="text-center">
-              <Button color="deep-orange">Sign up</Button>
-            </div>
-          </form>
 
           <dl>
             <dt>Multiple components subscribing to same redux store slice</dt>
